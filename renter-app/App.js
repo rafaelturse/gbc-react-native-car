07e47ce9bbf5fdfbb54b8d96cc60032a3d548750
Yuri Bittencourt <ysbittencourt@gmail.com>
2024-04-04 16:45:36 -0400

    final

--- renter-app/App.js
+++ renter-app/App.js
@@ -1,28 +1,30 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { UserContextProvider } from './utils/UserContext'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { UserContextProvider } from "./utils/UserContext";
 
-import Reservations from './view/Reservations'
-import Main from './view/Main'
-import Login from "./view/Login"
+import Reservations from "./view/Reservations";
+import Main from "./view/Main";
+import Login from "./view/Login";
+import { ReservationsContextProvider } from "./utils/ReservationsContext";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-
   return (
     <UserContextProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Main"
-            component={Main}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name="Reservations" component={Reservations} />
-          <Stack.Screen name="Login" component={Login} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <ReservationsContextProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Main"
+              component={Main}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Reservations" component={Reservations} />
+            <Stack.Screen name="Login" component={Login} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ReservationsContextProvider>
     </UserContextProvider>
-  )
+  );
 }
